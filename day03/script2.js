17342a343f4e444808538f6eddc7b53e02a7620c
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-16 10:59:52 -0600

    Solve 2nd part, include "aim" parameter

---
+++ day03/script2.js
@@ -0,0 +1,35 @@
+//Challenge
+//https://adventofcode.com/2021/day/2
+
+const jsonData = require('./data.json');
+const input = jsonData.split(' ');
+
+const directions = input.filter(item => !(parseInt(item) == item));
+const magnitude = input.filter(item => (parseInt(item) == item));
+
+let depth = 0, distance = 0, aim = 0, count = 0;
+
+for (let i = 0; i < directions.length; i++) {
+  count += 1;
+  switch (directions[i]) {
+    case 'forward':
+      distance += parseInt(magnitude[i]);
+      depth += parseInt(magnitude[i]) * aim;
+      break;
+    case 'up':
+      aim -= parseInt(magnitude[i]);
+      break;
+    case 'down':
+      aim += parseInt(magnitude[i]);
+      break;
+    default:
+      break;
+  }
+}
+
+console.log('magnitude', magnitude);
+console.log('directions', directions);
+
+
+console.log('distance', distance);
+console.log('depth', depth);
