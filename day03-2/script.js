27d86d1f4d5a3cc1f70e400de3a37a07135aad3a
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-16 18:31:34 -0600

    Import json data, specify run command in readme

---
+++ day03-2/script.js
@@ -0,0 +1,16 @@
+// Challenge
+// https://adventofcode.com/2021/day/3
+
+import jsonData from './data.json' assert {type: 'json'};
+const input = jsonData.split(' ');
+console.log(input);
+
+let gammaRate = {}, epsilonRate = {};
+gammaRate[0] = 'damn';
+
+console.log(gammaRate);
+
+// input.forEach(binaryStr => {
+// });
+
+// console.log(input[0].length);
