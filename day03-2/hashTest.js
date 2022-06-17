aa93ad08041cf3a890932ef7af831a081e6a7147
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-16 18:29:01 -0600

    Change class name to DecentHash, add assertions

--- day03-2/hashTest.js
+++ day03-2/hashTest.js
@@ -1,13 +1,24 @@
 // import _ from 'lodash';
 
-import { NaiveHashMap } from "./hashmap.js";
-// const assert = require('assert');
+import { DecentHashMap } from "./hashmap.js";
+import assert from "assert";
 
-const hashMap = new NaiveHashMap();
+const hashMap = new DecentHashMap();
 
 hashMap.set('cat', 2);
 hashMap.set('rat', 7);
 hashMap.set('dog', 1);
 hashMap.set('art', 8);
 
+console.log('collisions: ', hashMap.collisions);
 console.log(hashMap.buckets);
+
+console.log('cat: ', hashMap.get('cat'));
+console.log('rat: ', hashMap.get('rat'));
+console.log('dog: ', hashMap.get('dog'));
+console.log('art: ', hashMap.get('art'));
+
+assert.equal(hashMap.get('art'), 8);
+assert.equal(hashMap.get('cat'), 2);
+assert.equal(hashMap.get('rat'), 7);
+assert.equal(hashMap.get('dog'), 1);
