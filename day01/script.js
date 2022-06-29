fa6a4f4e0c8b3ca4d768f9e21dda6d36af97affe
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-29 15:23:24 -0600

    Add text to outputs of day01

--- day01/script.js
+++ day01/script.js
@@ -1,14 +1,14 @@
 //Challenge
 //https://adventofcode.com/2021/day/1
 
-var counter = 0;
-
 const jsonData = require('./data.json');
-
 let stringArray = jsonData.split(' ');
 
-for (let i = 0; i < stringArray.length-1; i++) {
-    +stringArray[i] < +stringArray[i + 1] ? counter++ : counter;
+let incrementCounter = 1;
+
+
+for (let i = 0; i < stringArray.length; i++) {
+  stringArray[i] > stringArray[i - 1] ? incrementCounter++ : incrementCounter;
 }
 
-console.log(counter);
+console.log("Increment Counter: " + incrementCounter);
